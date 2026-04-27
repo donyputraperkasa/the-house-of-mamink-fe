6db36630fb7fd846e5b2e3bd1a16539559cd1fc5
albertus magnus dony putra perkasa <donyputraperkasa@gmail.com>
2026-04-27 20:53:55 +0700

    debugging eror deploy 1

--- components/admin/layout/AdminContainer.tsx
+++ components/admin/layout/AdminContainer.tsx
@@ -8,16 +8,16 @@ export default function AdminContainer({ children }: { children: React.ReactNode
     const [isCollapsed, setIsCollapsed] = useState(false);
     return (
         <div className="min-h-screen bg-gray-50">
-        {/* Sidebar */}
-        <Sidebar isCollapsed={isCollapsed} setIsCollapsed={setIsCollapsed} />
+            {/* Sidebar */}
+            <Sidebar isCollapsed={isCollapsed} setIsCollapsed={setIsCollapsed} />
 
-        {/* Content */}
-        <div className={`relative transition-all duration-300 ${isCollapsed ? 'lg:ml-20' : 'lg:ml-64'}`}>
-            <div className="px-6 py-8 space-y-6">
-                <BreadCrumbs  />
-            {children}
+            {/* Content */}
+            <div className={`relative transition-all duration-300 ${isCollapsed ? 'lg:ml-20' : 'lg:ml-64'}`}>
+                <div className="px-6 py-8 space-y-6">
+                    <BreadCrumbs  />
+                {children}
+                </div>
             </div>
-        </div>
         </div>
     );
 }
--- components/admin/layout/SidebarFooter.tsx
+++ components/admin/layout/SidebarFooter.tsx
@@ -5,12 +5,11 @@ export default function SidebarFooter({ isCollapsed, setIsOpen }: any) {
         <div className="p-4 border-t border-gray-200">
             <button
                 className={`bg-red-100 flex items-center ${
-                isCollapsed ? 'justify-center py-4' : 'gap-3 px-4 py-3'
-                } w-full rounded-xl text-red-600 font-semibold hover:bg-red-500 hover:text-white transition shadow-sm`}
-                onClick={() => {
-                setIsOpen(false);
-                localStorage.removeItem('token');
-                window.location.href = '/admin-login';
+                    isCollapsed ? 'justify-center py-4' : 'gap-3 px-4 py-3'} w-full rounded-xl text-red-600 font-semibold hover:bg-red-500 hover:text-white transition shadow-sm`}
+                    onClick={() => {
+                    setIsOpen(false);
+                    localStorage.removeItem('token');
+                    window.location.href = '/admin-login';
                 }}
             >
                 <LogOut size={20} className="min-w-[20px]" />
